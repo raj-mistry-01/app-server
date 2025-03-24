a63bd7875c8262ba8e2ca9a689ac9edf472dfc31
Makwana Raj <151895608+raj-mistry-01@users.noreply.github.com>
2025-03-24 09:34:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 // Store the ngrok URL in-memory (for now, you can enhance this with a database if needed)
-let ngrokUrl = ' https://a91d-2409-40c1-e-f319-a1db-42f4-3852-b2db.ngrok-free.app';
+let ngrokUrl = 'https://a91d-2409-40c1-e-f319-a1db-42f4-3852-b2db.ngrok-free.app';
 
 // Endpoint to get the latest ngrok URL
 app.get('/ngrok-url', (req, res) => {
